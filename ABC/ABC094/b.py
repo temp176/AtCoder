30dcf3bd766bf215bfee998ede4d5e22760334cc
temp176 <teheodks@gmail.com>
2018-06-06 09:57:44 +0900

    no message

--- ABC/ABC094/b.py
+++ ABC/ABC094/b.py
@@ -1,3 +1,14 @@
-N, M, X = (int(i) for i in input().split())  
+N,M,X = map(int,input().split())
 A = [int(i) for i in input().split()]
+
+counterToN,counterToX = 0,0
+
+for i in A:
+    if i - 1 < X :
+        counterToN += 1
+    else :
+        counterToX += 1
+
+print(min([counterToN,counterToX]))
+
 
