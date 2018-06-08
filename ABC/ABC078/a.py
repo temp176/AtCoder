f3a10adb04ad69c7070abb1066ccda84b2565359
temp176 <teheodks@gmail.com>
2018-06-08 13:07:36 +0900

    no message

---
+++ ABC/ABC078/a.py
@@ -0,0 +1,7 @@
+X,Y=[i for i in input().split()]
+if X < Y:
+    print('<')
+elif X > Y:
+    print('>')
+else:
+    print('=')
---
+++ ABC/ABC078/b.py
@@ -0,0 +1,3 @@
+X,Y,Z = map(int,input().split())
+
+print(int((X-Z)/(Y+Z)))
